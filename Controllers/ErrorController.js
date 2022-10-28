abeaaa10b7f3179c62fafb398b30a359689244a5
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-28 10:27:38 +0100

    Update ErrorController.js

--- Controllers/ErrorController.js
+++ Controllers/ErrorController.js
@@ -4,7 +4,18 @@ module.exports = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'Error';
 
+	const handleDevError = (err, res) => {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message,
+			// err: err,
+		});
+	};
+
 	if (process.env.NODE_ENV === 'development') {
+		let error = { ...err };
+		handleDevError(err, res);
+	} else if (process.env.NODE_ENV === 'production') {
 		let Error = { ...err };
 	}
 };
