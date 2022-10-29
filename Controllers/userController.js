73bb57fc3433b58277678b2b41e19bc9073e5afd
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-29 17:48:28 +0100

    Update userController.js

---
+++ Controllers/userController.js
@@ -0,0 +1,25 @@
+const userModel = require('../Models/userModel');
+const catchAsync = require('../utils/CatchAsync');
+const AppRes = require('../utils/AppResponse');
+const AppError = require('../utils/AppError');
+
+// uiniversal User variable
+var User;
+
+// delete a User
+exports.deleteUser = catchAsync(async (req, res, next) => {
+	User = await userModel.findByIdAndDelete(req.params.id);
+
+	if (!User) return next(new AppError('User not Found', 404));
+
+	new AppRes(res, User, 200);
+});
+
+// update user
+exports.updateUser = catchAsync(async (req, res) => {
+	User = await userModel.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+	new AppRes(res, User, 200);
+});
