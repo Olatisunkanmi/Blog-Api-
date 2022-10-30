269839c12240724d1fdf81c2a7e30f3842ae88d8
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-30 18:10:08 +0100

    Update auth.js

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -2,9 +2,17 @@ const userModel = require('../Models/userModel');
 const AppError = require('../utils/AppError');
 const AppRes = require('../utils/AppResponse');
 const catchAsync = require('../utils/AppResponse');
+const jwt = require('jsonwebtoken');
 
 // universal User variable
 var Users;
+
+// Sign Token Fn.
+const SIGNTOKEN = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+};
 
 //  Register
 exports.Register = catchAsync(async (req, res, next) => {
