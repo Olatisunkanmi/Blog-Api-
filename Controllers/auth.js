be21bd886f042ce649ff882cfe7d2572ee871652
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-31 02:38:36 +0100

    Update auth.js

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,11 +1,11 @@
 const userModel = require('../Models/userModel');
 const AppError = require('../utils/AppError');
 const AppRes = require('../utils/AppResponse');
-const catchAsync = require('../utils/AppResponse');
+const catchAsync = require('../utils/CatchAsync');
 const jwt = require('jsonwebtoken');
 
 // universal User variable
-var Users;
+var User;
 
 // Sign Token Fn.
 const SIGNTOKEN = (id) => {
@@ -16,18 +16,43 @@ const SIGNTOKEN = (id) => {
 
 //  Register
 exports.Register = catchAsync(async (req, res, next) => {
-	const { username, password, email } = req.body;
+	const { firstName, lastName, password, email } = req.body;
 
-	if ((!username, password, email)) {
+	console.log(req.body);
+
+	if (!firstName || !lastName || !email || !password) {
 		return next(new AppError('Missing Parameters '));
 	}
 
-	Users = await userModel.create({
-		username: username,
+	User = await userModel.create({
+		firstName: firstName,
+		lastName: lastName,
 		password: password,
 		email: email,
 	});
 
-	const TOKEN = SIGNTOKEN(Users._id);
-	new ResponseHandler(res, Users, 201);
+	const TOKEN = SIGNTOKEN(User._id);
+	new AppRes(res, User, 201, TOKEN);
+});
+
+// Login
+exports.Login = catchAsync(async (req, res, next) => {
+	const { password, email } = req.body;
+
+	if (!email || !password) {
+		return new AppError('Please Provide email and Password', 400);
+	}
+
+	User = await userModel.findOne({ email }).select('+ password');
+
+	console.log(User);
+	// if (
+	// 	!User ||
+	// 	!(await User.correctPassword(password, User.password))
+	// ) {
+	// 	return next(new AppError('Incorrect email or password', 401));
+	// }
+
+	const TOKEN = SIGNTOKEN(User._id);
+	new AppRes(res, User, 200, TOKEN);
 });
