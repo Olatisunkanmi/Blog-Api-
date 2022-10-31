4230ea45d342d1a15c4a4773212cf9c2a624eac7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-31 01:03:51 +0100

    Update postRoute.js

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -8,4 +8,8 @@ router
 
 router.route('/publish/:id').post(postController.publishPosts);
 
+router.route('/:id').post(postController.getAPost);
+
+router.route('/:id').delete(postController.deletePosts);
+
 module.exports = router;
