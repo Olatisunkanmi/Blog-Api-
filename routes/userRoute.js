80f00d5841d80ebfacf4bbd17db6dc386746576a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-10-31 02:38:25 +0100

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userController = require('../Controllers/userController');
+const { Register, Login } = require('../Controllers/auth');
 
-// router.route('/').get(userController.getUsers).post(userController)
+router.route('/signup').post(Register);
+router.route('/login').post(Login);
+
+router.route('/').get(userController.getUsers);
 
 module.exports = router;
